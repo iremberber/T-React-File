5ae6fde6f39ab93e12f591d6a702f9d75db6955f
irem <iremberber0@gmail.com>
2024-09-22 19:59:25 +0300

    t react lesson 2

---
+++ hello-react/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Login(){
+    return(
+        <div>
+            Kullanıcı adınızı giriniz
+            <input placeholder="kullanıcı adınızı giriniz"/>
+
+
+
+        </div>
+    )
+}
+
+export default Login;
