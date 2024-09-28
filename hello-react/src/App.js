a9b34e8c60a752eaa14472f3463ada12c58e8cbd
irem <iremberber0@gmail.com>
2024-09-28 14:46:20 +0300

    useState Object

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
 
-import Counter from "./components/Counter";
+import User from "./components/user";
 
 
 function App() {
   return(  
     <div >
-      <Counter/>
-      {/* 
-      <User
-      //title="kişi"
-       name="irem"
-       city="61"
-       age="21"  
-   
-       data={{
-        name: "İrem Berber", 
-        city: "Trabzon",
-        age: 21  // age should be a number, not a string
-      }}
-      family={["Ayşegül", "Furkan", "Mustafa"]}
-       />  
-      */}
+      <User/>
+ 
   </div>
 );
 }
--- hello-react/src/components/user.js
+++ hello-react/src/components/user.js
@@ -1,76 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
-/*function user(props) {
-    console.log(props);
-  return  <div>
-      user
-      <div>isim= {props.data.name}</div>
-      <div>şehir= {props.data.city}</div>
-      <div>yaş= {props.data.age}</div>
+function User() {
+  const names = ["Ayşe", "Furkan", "Mustafa", "Ali", "Veli", "Fatma"];
+  const surnames = ["Berber", "Sahin", "Makas", "Klavye"]
+  const [user, setUser] = useState({ name: 'İrem', surname: 'Berber' });
+
+  const getRandomName = () => {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    return names[randomIndex];
+  };
+
+  const getRandomSurname = () => {
+    const randomIndexTwo = Math.floor(Math.random()* surnames.length);
+    return surnames[randomIndexTwo];
+  }
+
+  return (
+    <div>
+      <h2>user</h2>
+      {user.name} {user.surname}
+      
+      <button onClick={() => setUser({ ...user, name: getRandomName() })}>
+        İsim Değiştirici
+      </button>
+
+      <button onClick={() => setUser({ ...user, surname: getRandomSurname() })}>
+        Soyisim Değiştirici
+      </button>
     </div>
-  
-}*/
-
-
-/*function user({data: {name, city, age}}) {
-  return  <div>
-      user
-      <div>isim= {name}</div>
-      <div>şehir= {city}</div>
-      <div>yaş= {age}</div>
-    </div>
-  
-}*/
-
-/*function user(props) {
-    console.log(props);
-
-    const {name, age, city} = props.data;
-
-  return  <div>
-      user
-      <div>isim= {name}</div>
-      <div>şehir= {city}</div>
-      <div>yaş= {age}</div>
-    </div>
-  
-}*/
-
-
-function user({title, data, family}) {
-    
-  return  <div>
-      <strong>{title}</strong>
-      <div>isim= {data.name}</div>
-      <div>şehir= {data.city}</div>
-      <div>yaş= {data.age}</div>
-
-       <hr/>
-     <h3>Family members</h3>
-     {family.map((fam, i) => (
-        <div key={i}>{fam}</div>)
-      )} 
-    </div>
-  
+  );
 }
 
-user.propTypes= {
-/*  name: PropTypes.string,
-  city: PropTypes.string.isRequired,
-  age: PropTypes.number, */
-
-  data: PropTypes.exact({
-    title: PropTypes.string.isRequired,
-    name: PropTypes.string,
-    age: PropTypes.number,
-    city: PropTypes.string,
-  }),
-  family: PropTypes.string
-};
-
-user.defaultProps = {
-  title: 'Kişi',
-}
-
-export default user;
+export default User;
