aba0a6fa510a78d3237a5d8200d66deb2817d286
irem <iremberber0@gmail.com>
2024-10-08 21:29:40 +0300

    Form Elemanları için State Kullanımı - 2

--- hello-react/src/components/Form.js
+++ hello-react/src/components/Form.js
@@ -2,28 +2,32 @@ import React, {useState} from 'react'
 
 function Form() {
 
-    const[name, setName] = useState("");
-    const[surname, setSurname] = useState("");
-    const[gender, setGender] = useState("1");
+    const [form, setForm] = useState({name:"", surname:"", gender:"0"});
+
+    const handleChange= (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+
   return (
     <div>
       <div>İsim</div>
-      <input placeholder='isim' value={name} onChange={(event) => setName(event.target.value)}/>
-      <input placeholder='soyisim' value={surname} onChange={(event) => setSurname(event.target.value)}/>
+      <input placeholder='isim' value={form.name} onChange={handleChange}/>
+      <input placeholder='soyisim' value={form.surname} onChange={handleChange}/>
       <div>
-         <select value={gender} onChange={(event) => setGender(event.target.value)}>
+         <select value={form.gender} onChange={handleChange}>
             <option value={0}>erkek</option>
             <option value={1}>kadın</option>
          </select>
        </div>
 <hr/>
       <div>İsim:
-        <strong>{name}</strong></div>
+        <strong>{form.name}</strong></div>
       
-      <div>Soyisim:<strong>{surname}</strong></div>
+      <div>Soyisim:<strong>{form.surname}</strong></div>
 
     <br/>
-      Cinsiyet:<strong>{gender ==='0' ? "erkek" : "kadın"}</strong>
+      Cinsiyet:<strong>{form.gender ==='0' ? "erkek" : "kadın"}</strong>
       
       
     </div>
